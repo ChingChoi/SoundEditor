9a4fa3eaf6628b58d565673723f1c07d3e14de3b
Ching Choi <chingchoi128@gmail.com>
2018-05-04 00:17:02 -0700

    mergingMerge branch 'master' of https://github.com/ChingChoi/SoundEditor

---
+++ SoundEditorOptimize/PerformanceProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SoundEditorOptimize
+{
+    class PerformanceProfile
+    {
+        public static long Profile(Action method)
+        {
+            Stopwatch st = Stopwatch.StartNew();
+            method();
+            st.Stop();
+            return st.ElapsedMilliseconds;
+        }
+    }
+}
